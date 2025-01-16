e371ddf9242ccef790c3b61d9ed4dffe11aac9c7
ryolingo <ryomatsuya04@gmail.com>
2025-01-16 10:21:54 +0900

    Initial Commit

---
+++ BusClock/BusClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusClockApp.swift
+//  BusClock
+//
+//  Created by matsumotoryota on 2025/01/16.
+//
+
+import SwiftUI
+
+@main
+struct BusClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
